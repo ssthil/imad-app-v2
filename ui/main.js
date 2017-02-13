dd965d2f4962a562490dacc73ea5aa3a11ae62e3
Senthil Sivaramakrishnan <ssthil@gmail.com>
2017-02-13 18:38:00 +0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,17 +22,32 @@ button.onclick = function () {
     request.send(null);
 };
 
+//Submit name
 var nameInput = document.getElementById('name');
 var listname = nameInput.value;
 var submit = document.getElementById('submit_btn');
 
 submit.onclick = function () {
-    var names = ['name1', 'name2', 'name3'];
-    var list = '';
     
-    for (var i=0; i < names.length; i++) {
-        list += '<li>'+names[i]+'</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list = '';
+                
+                for (var i=0; i < names.length; i++) {
+                    list += '<li>'+names[i]+'</li>';
+                }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list; 
+            }
+        }
+    };
+    //make the request
+    request.open('GET', 'http://ssthil.imad.hasura-app.io/submit-name?name='+listname, true);
+    request.send(null);
 };

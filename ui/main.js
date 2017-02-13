3a63864ef7d0e20dcf273aa04707741ff234de72
Senthil Sivaramakrishnan <ssthil@gmail.com>
2017-02-13 18:13:46 +0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -31,7 +31,7 @@ submit.onclick = function () {
     var list = '';
     
     for (var i=0; i < names.length; i++) {
-        list = '<li>'+listname[i]+'</li>';
+        list = '<li>'+names[i]+'</li>';
     }
     var ul = document.getElementById('namelist');
     ul.innerHTML = list;

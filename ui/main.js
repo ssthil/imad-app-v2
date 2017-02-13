63aec937df3f1d52d1139077b0bdcca1cda1b2be
Senthil Sivaramakrishnan <ssthil@gmail.com>
2017-02-13 16:47:06 +0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,22 @@ var button = document.getElementById("counter");
 var counter = 0;
 
 button.onclick = function () {
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    
+    var request = new XMLHttpRequest();
+    
+    //capture the response and store it in a variable
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            //take some action
+            if (request.status === 200) {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+        //not done yet
+    }
+    //make the request
+    request.open('GET', 'http://ssthil.imad.hasura-app.io/counter', true);
+    request.send(null);
 }

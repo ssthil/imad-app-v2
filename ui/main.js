c538655d501696bfff7686f8c28817679bb6bd36
Senthil Sivaramakrishnan <ssthil@gmail.com>
2017-02-13 15:55:33 +0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,9 @@
-console.log('Loaded!');
+//Counter code
+var button = document.getElementById("counter");
+var counter = 0;
+
+button.onclick = function () {
+    counter = counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
+}

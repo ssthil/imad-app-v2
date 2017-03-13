05a6b0c53b08f93f39905b07c6829dca5a617fb1
Senthil Sivaramakrishnan <ssthil@gmail.com>
2017-03-13 16:06:10 +0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -36,9 +36,9 @@ app.get('/test-db', function(req, res){
 });
 
 app.get('/customers', function(req, res) {
-    pool.query("SELECT * FROM customers", function(err, result){
+    pool.query("SELECT * FROM test", function(err, result){
         if(err) {
-            res.status(500).send(err.toString()); 
+            res.status(500).send(err.toString());
         } else {
             res.send(JSON.stringify(result));
         }

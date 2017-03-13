224849955b158d52c46c9113ea296a8efda224e0
Senthil Sivaramakrishnan <ssthil@gmail.com>
2017-03-13 16:07:01 +0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.get('/customers', function(req, res) {
         if(err) {
             res.status(500).send(err.toString());
         } else {
-            res.send(JSON.stringify(result));
+            res.send(JSON.stringify(result.rows));
         }
     })
 });
